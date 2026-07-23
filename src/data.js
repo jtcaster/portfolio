eb69a8f9775bc82bcf71a7c4bfedc323492bb9f8
Jonas Caster <jtcaster02@gmail.com>
2026-07-23 12:42:35 -0400

    fix photos

--- src/data.js
+++ src/data.js
@@ -47,32 +47,32 @@ export const items = [
 
   export const photos = [
     {
-      src: "/public/img1.png",
+      src: "/img1.png",
       title: "A1 for Ampitheater Worship @ Chautauqua Institution",
       description: "CL5, multi-mic panel, monitors"
     },
     {
-      src: "/public/img2.png",
+      src: "/img2.png",
       title: "Stage Setup @ Chautauqua Institution",
       description: "Lighting, line arrays"
     },
     {
-      src: "/public/img3.png",
+      src: "/img3.png",
       title: "Staff Meeting @ Chautauqua Institution",
       description: "ATEM, CAT connected PTZ, streamed via Microsoft Teams"
     },
     {
-        src: "/public/img4.png",
+        src: "/img4.png",
         title: "Videography @ Iglesia Adventista Hispana de la Comunidad, Chattanooga TN",
         description: "Sony FX30, shot for social media content of worship service"
       },
       {
-        src: "/public/img5.png",
+        src: "/img5.png",
         title: "A1 for Iglesia Adventista Hispana de la Comunidad",
         description: "SQ7, full band"
       },
       {
-        src: "/public/img6.png",
+        src: "/img6.png",
         title: "A1 for Gym Masters Home Show @ Southern Adventist University",
         description: "QL5, audio playback"
       }
